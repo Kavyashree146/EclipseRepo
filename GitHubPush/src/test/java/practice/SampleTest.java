f7e100374cb6f4cf000977f534eecbecf2ce3009
Kavyashree146 <kavyakundapur1999@gmail.com>
2024-03-09 11:04:57 +0530

    GitHub Push

---
+++ GitHubPush/src/test/java/practice/SampleTest.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+ 
+	@Test
+	public void toLearnGithub() {
+		Reporter.log("To practice git hub", true);
+	}
+}
