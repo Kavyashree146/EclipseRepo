200c20329df92b515a737eea307f97f34c37e8b7
Kavyashree146 <kavyakundapur1999@gmail.com>
2024-03-09 11:07:14 +0530

    Added class

---
+++ GitHubPush/src/test/java/practice/ToRun.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToRun {
+
+	@Test
+	public void testGithub() {
+		Reporter.log("hi",true);
+	}
+}
